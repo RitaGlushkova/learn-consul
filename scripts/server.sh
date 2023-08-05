1ac39667ee5d5c7a4ad9090f15a72a2d6e512002
Margarita Glushkova <93099190+RitaGlushkova@users.noreply.github.com>
2023-08-05 15:03:45 +0100

    load set up via script

--- scripts/server.sh
+++ scripts/server.sh
@@ -1,3 +1,37 @@
 #!/bin/bash
 
 echo "Hello Consul Server!"
+
+# Install Consul.  This creates...
+# 1 - a default /etc/consul.d/consul.hcl
+# 2 - a default systemd consul.service file
+sudo apt update && sudo apt install gpg
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+gpg --no-default-keyring --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg --fingerprint
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update
+sudo apt install consul=1.10.1
+
+# Grab instance IP
+local_ip=`ip -o route get to 169.254.169.254 | sed -n 's/.*src \([0-9.]\+\).*/\1/p'`
+
+# Modify the default consul.hcl file
+cat > /etc/consul.d/consul.hcl <<- EOF
+data_dir = "/opt/consul"
+
+client_addr = "0.0.0.0"
+
+ui_config{
+  enabled = true
+}
+server = true
+
+bind_addr = "0.0.0.0"
+
+advertise_addr = "$local_ip"
+
+bootstrap_expect=1
+EOF
+
+# Start Consul
+sudo systemctl start consul
